cd3dafd7fe2e3a11eb0ec10429096778c2fcc3c7
Uday123455 <89934216+Uday123455@users.noreply.github.com>
2025-10-26 08:11:28 +0000

    eventlisteners,buttons and functions added

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-
+let cell=16;
 const control=document.querySelector(".control");
+const length=512;
 
 
 
-for(let i=0;i<100;i++){
+
+control.addEventListener("mouseover", (event) => {
+  event.target.style.backgroundColor = "orange";
+});
+
+function enter(){
+    cell=prompt("enter your number");
+    if(cell<101){
+         clean();
+    creategrid();
+    }
+}
+function clean(){
+    control.innerHTML="";
+}
+function kala(){
+    
+    clean();
+    creategrid();
+}
+function creategrid(){
+    for(let i=0;i<cell*cell;i++){
     const newdiv=document.createElement("div");
     newdiv.className="pixel";
+    newdiv.style.height=`${length/cell}px`;
+    newdiv.style.width=`${length/cell}px`
     control.appendChild(newdiv);
 };
+}
+
+
+creategrid();
